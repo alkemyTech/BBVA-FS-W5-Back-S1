e81f58f8e29831159aea52424ab8804485c4d1e8
Benetti <O013767@arg.bbva.com>
2024-11-19 19:36:26 -0300

    creacion del modelo roles

---
+++ src/main/java/com/BBVA/DiMo_S1/D_models/Rol.java
@@ -0,0 +1,39 @@
+package com.BBVA.DiMo_S1.D_models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Roles")
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    @NotNull(message = "el nombre no debe estar nulo")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(updatable = false)
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
+    private Date creationDate;
+
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
+    private Date updateDate;
+
+    @PrePersist
+    protected void onCreate(){
+        this.creationDate = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updateDate = new Date();
+    }
+}
