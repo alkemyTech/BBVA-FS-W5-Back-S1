9b65290390cee5a41ed68e7993cc8602372ed94f
Juan Manuel Noli <juanmanuelnoli03@gmail.com>
2024-11-19 17:55:49 -0300

    Subimos configuracion base del proyecto

---
+++ src/test/java/com/BBVA/DiMo_S1/DiMoS1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BBVA.DiMo_S1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiMoS1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
