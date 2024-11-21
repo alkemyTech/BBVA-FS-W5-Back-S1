dc210f1c6edb9e9fb098f61a120c5979a4fd23cb
LuccaTrovato-2006 <O013771@arg.bbva.com>
2024-11-21 14:24:23 -0300

    Modificamos DTO para endpoint de crearAccount

--- src/main/java/com/BBVA/DiMo_S1/D_dtos/accountDTO/AccountDTO.java
+++ src/main/java/com/BBVA/DiMo_S1/D_dtos/accountDTO/AccountDTO.java
@@ -1,5 +1,8 @@
 package com.BBVA.DiMo_S1.D_dtos.accountDTO;
 
+import com.BBVA.DiMo_S1.D_dtos.userDTO.UserDTO;
+import com.BBVA.DiMo_S1.D_models.Account;
+import com.BBVA.DiMo_S1.D_models.User;
 import com.BBVA.DiMo_S1.E_constants.Enums.CurrencyType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,10 +19,12 @@ public class AccountDTO {
     private double transactionLimit;
     private double balance ;
 
-    public AccountDTO(CurrencyType currency) {
-        this.currency = currency;
-        this.balance = 0;
-        this.transactionLimit = (currency == CurrencyType.ARS) ? 300000 : 1000;
+
+    public AccountDTO fromAccount(final Account account){
+        this.currency = account.getCurrency();
+        this.transactionLimit = account.getTransactionLimit();
+        this.balance = account.getBalance();
+        return this;
     }
 
 
