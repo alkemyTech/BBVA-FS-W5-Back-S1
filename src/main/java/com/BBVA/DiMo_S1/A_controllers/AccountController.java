1a8bea58034af4592a1cffa737e785c62e1b891c
Juan Manuel Noli <juanmanuelnoli03@gmail.com>
2024-11-21 12:12:53 -0300

    Modificamos AccountController y UserController

--- src/main/java/com/BBVA/DiMo_S1/A_controllers/AccountController.java
+++ src/main/java/com/BBVA/DiMo_S1/A_controllers/AccountController.java
@@ -1,7 +1,7 @@
 package com.BBVA.DiMo_S1.A_controllers;
 
 
-import com.BBVA.DiMo_S1.B_services.implementations.AccountServiceImpl;
+import com.BBVA.DiMo_S1.B_services.implementations.AccountServiceImplementation;
 import com.BBVA.DiMo_S1.D_models.Account;
 import com.BBVA.DiMo_S1.E_exceptions.CustomException;
 import jakarta.validation.Valid;
@@ -19,14 +19,14 @@ import org.springframework.web.bind.annotation.*;
 public class AccountController {
 
     @Autowired
-    AccountServiceImpl accountServiceImpl;
+    AccountServiceImplementation accountServiceImplementation;
 
     //1- softDelete de un User de la BD.
     @DeleteMapping("/{idAccount}")
     public ResponseEntity<String> softDelete (@Valid @PathVariable @NotNull
             (message = "El ID de la Account no puede ser nulo.") long idAccount) throws CustomException {
 
-        accountServiceImpl.softDelete(idAccount);
+        accountServiceImplementation.softDelete(idAccount);
 
         return ResponseEntity.ok().body("Account con ID = " + idAccount + " eliminado con éxito!");
     }
@@ -34,7 +34,7 @@ public class AccountController {
     //2- createAccount en la BD
     @PostMapping("/create")
     public ResponseEntity<Account> createAccount(@Valid @RequestBody Account account){
-        Account accountCreada = accountServiceImpl.createAccount(account);
+        Account accountCreada = accountServiceImplementation.createAccount(account);
         return ResponseEntity.status(HttpStatus.CREATED).body(accountCreada);
     }
 
