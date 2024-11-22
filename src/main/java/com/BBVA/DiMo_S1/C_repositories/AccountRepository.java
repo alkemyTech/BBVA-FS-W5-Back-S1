1c6462055026ce6619aeb70bef54bff1fdf1371c
Juan Manuel Noli <juanmanuelnoli03@gmail.com>
2024-11-22 10:55:27 -0300

    Creamos metodo para traer Account por CBU

--- src/main/java/com/BBVA/DiMo_S1/C_repositories/AccountRepository.java
+++ src/main/java/com/BBVA/DiMo_S1/C_repositories/AccountRepository.java
@@ -21,6 +21,11 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     @Query("SELECT ac FROM Account ac JOIN FETCH ac.user u JOIN FETCH u.role WHERE u.id = :idUser")
     List<Account> getByIdUser(@Param("idUser") long idUser);
 
+    @Query("SELECT ac FROM Account ac JOIN FETCH ac.user u JOIN FETCH u.role WHERE u.id = :idUser AND ac.currency = 1")
+    Optional <Account> getUsdAccountByIdUser(@Param("idUser") long idUser);
+
+    Optional<Account> findByCbu (String cbu);
+
 
 
 
