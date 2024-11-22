403cb5a7c38181d350fa589f440c5063c1652a40
Juan Manuel Noli <juanmanuelnoli03@gmail.com>
2024-11-22 10:56:02 -0300

    Creamos nuevo DTO de transactions

--- src/main/java/com/BBVA/DiMo_S1/D_dtos/transactionDTO/TransactionDTO.java
+++ src/main/java/com/BBVA/DiMo_S1/D_dtos/transactionDTO/TransactionDTO.java
@@ -1,6 +1,8 @@
-package com.BBVA.DiMo_S1.D_dtos.TransactionDTO;
+package com.BBVA.DiMo_S1.D_dtos.transactionDTO;
 
+import com.BBVA.DiMo_S1.D_models.Transaction;
 import com.BBVA.DiMo_S1.E_constants.Enums.TransactionType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,15 +16,21 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class TransactionDTO {
 
+    @JsonIgnore
     private Long id;
 
     private Double amount;
-
     private TransactionType type;
-
     private String description;
-
     private LocalDateTime transactionDate;
+    private String cuentaDestino;
 
-    private Long accountId; // Clave foránea
+    public TransactionDTO (Transaction transaction) {
+        this.id = transaction.getId();
+        this.amount = transaction.getAmount();
+        this.type = transaction.getType();
+        this.description = transaction.getDescription();
+        this.transactionDate = transaction.getTransactionDate();
+        this.cuentaDestino = transaction.getAccount().getCbu();
+    }
 }
