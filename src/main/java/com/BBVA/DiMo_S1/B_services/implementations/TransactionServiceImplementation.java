865dc73007df1d5951faacbaf24ebb1dabc12b14
Juan Manuel Noli <juanmanuelnoli03@gmail.com>
2024-11-22 10:53:32 -0300

    Creamos logica para envio de dinero en USD

--- src/main/java/com/BBVA/DiMo_S1/B_services/implementations/TransactionServiceImplementation.java
+++ src/main/java/com/BBVA/DiMo_S1/B_services/implementations/TransactionServiceImplementation.java
@@ -3,24 +3,39 @@ package com.BBVA.DiMo_S1.B_services.implementations;
 import com.BBVA.DiMo_S1.B_services.interfaces.TransactionService;
 import com.BBVA.DiMo_S1.C_repositories.AccountRepository;
 import com.BBVA.DiMo_S1.C_repositories.TransactionRepository;
-import com.BBVA.DiMo_S1.D_dtos.TransactionDTO.TransactionDTO;
-import com.BBVA.DiMo_S1.D_models.Enums.TransactionType;
+import com.BBVA.DiMo_S1.C_repositories.UserRepository;
+import com.BBVA.DiMo_S1.D_dtos.transactionDTO.SimpleTransactionDTO;
+import com.BBVA.DiMo_S1.D_dtos.transactionDTO.TransactionDTO;
+import com.BBVA.DiMo_S1.D_dtos.userDTO.UserSecurityDTO;
+import com.BBVA.DiMo_S1.D_models.Account;
+import com.BBVA.DiMo_S1.E_config.JwtService;
+import com.BBVA.DiMo_S1.E_constants.Enums.CurrencyType;
+import com.BBVA.DiMo_S1.E_constants.Enums.TransactionType;
 import com.BBVA.DiMo_S1.D_models.Transaction;
+import com.BBVA.DiMo_S1.E_constants.ErrorConstants;
+import com.BBVA.DiMo_S1.E_exceptions.CustomException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class TransactionServiceImplementation implements TransactionService {
 
-    private final TransactionRepository transactionRepository;
-    private final AccountRepository accountRepository;
+    @Autowired
+    private TransactionRepository transactionRepository;
 
     @Autowired
-    public TransactionServiceImplementation(TransactionRepository transactionRepository, AccountRepository accountRepository) {
-        this.transactionRepository = transactionRepository;
-        this.accountRepository = accountRepository;
-    }
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtService jwtService;
 
     @Override
     public void sendArs(TransactionDTO transactionDTO, String token) {
@@ -38,7 +53,7 @@ public class TransactionServiceImplementation implements TransactionService {
         }
 
         // Verificar que la cuenta destinataria exista
-        var recipientAccount = accountRepository.findById(transactionDTO.getAccountId())
+        var recipientAccount = accountRepository.findByCbu(transactionDTO.getCuentaDestino())
                 .orElseThrow(() -> new IllegalArgumentException("Cuenta receptora no encontrada."));
 
         // Crear y guardar la transacción de "payment" para el emisor
@@ -68,4 +83,81 @@ public class TransactionServiceImplementation implements TransactionService {
         accountRepository.save(recipientAccount);
 }
 
+    public TransactionDTO sendUsd (HttpServletRequest request, SimpleTransactionDTO simpleTransactionDTO) {
+
+        //Inicializamos la transaccion vacia.
+        Transaction transaction = Transaction.builder().build();
+
+        //Extraemos el User autenticado.
+        UserSecurityDTO userSecurityDTO = jwtService.validateAndGetSecurity(jwtService.extraerToken(request));
+
+        //Antes de realizar cualquier tipo de validación, validamos: Si el User autenticado tiene cuenta en USD,
+        //si el balance es suficiente y si el monto es correcto.
+        Optional <Account> accountEmisora = accountRepository.getUsdAccountByIdUser(userSecurityDTO.getId());
+
+        if (accountEmisora.isEmpty() || simpleTransactionDTO.getAmount() > accountEmisora.get().getTransactionLimit()
+        || accountEmisora.get().getBalance() < simpleTransactionDTO.getAmount()) {
+
+            throw new CustomException(HttpStatus.CONFLICT, ErrorConstants.ERROR_EN_TRANSACCION);
+        }
+
+        //Si la cuenta esta en condiciones...
+
+        //Obtenemos la cuenta destino. Verificamos 2 cosas: Que el CBU no sea el del usuario autenticado
+        //y que exista.
+        Optional <Account> accountBuscada = accountRepository.findByCbu(simpleTransactionDTO.getCBU());
+
+        //Si la cuenta buscada existe...
+        if (accountBuscada.isPresent()) {
+
+            //La asignamos a una cuenta...
+            Account accountDestino = accountBuscada.get();
+
+            //Verificamos que el id del User de la cuenta sea diferente al del usuario autenticado.
+            if (accountDestino.getUser().getId() != userSecurityDTO.getId()) {
+
+                //Verificamos el tipo de cuenta a la cual se desea enviar el dinero..
+                if (accountDestino.getCurrency().equals(CurrencyType.USD)) {
+
+                    //Realizamos transferencia...
+
+                    //Le creamos un income a la cuenta destino:
+                    double balanceConIncome = accountDestino.getBalance() + simpleTransactionDTO.getAmount();
+                    accountDestino.setBalance(balanceConIncome);
+
+                    //Le creamos un payment a la cuenta emisora:
+                    double balanceConPayment = accountEmisora.get().getBalance() - simpleTransactionDTO.getAmount();
+                    accountEmisora.get().setBalance(balanceConPayment);
+
+                    //Actualizamos las cuentas en la BD.
+                    accountRepository.save(accountDestino);
+                    accountRepository.save(accountEmisora.get());
+
+                    //Seteamos los datos de la transaccion
+                    transaction.setDescription(simpleTransactionDTO.getDescription());
+                    transaction.setAmount(simpleTransactionDTO.getAmount());
+                    transaction.setType(TransactionType.payment);
+                    transaction.setAccount(accountDestino);
+
+                } else {
+
+                    throw new CustomException(HttpStatus.FORBIDDEN, ErrorConstants.ACCOUNT_NO_VALIDA);
+                }
+
+            } else  {
+
+                throw new CustomException(HttpStatus.FORBIDDEN, ErrorConstants.ERROR_CUENTA_PROPIA);
+            }
+        } else {
+
+            throw new CustomException(HttpStatus.NOT_FOUND, ErrorConstants.ACCOUNT_NO_ENCONTRADA);
+        }
+
+        //Si sale bien la transaccion, la guardamos en la BD.
+        Transaction transactionGuardada = transactionRepository.save(transaction);
+
+        TransactionDTO transactionDTO = new TransactionDTO(transactionGuardada);
+
+        return transactionDTO;
+    }
 }
--- src/main/java/com/BBVA/DiMo_S1/B_services/interfaces/TransactionService.java
+++ src/main/java/com/BBVA/DiMo_S1/B_services/interfaces/TransactionService.java
@@ -1,9 +1,13 @@
 package com.BBVA.DiMo_S1.B_services.interfaces;
 
-import com.BBVA.DiMo_S1.D_dtos.TransactionDTO.TransactionDTO;
+import com.BBVA.DiMo_S1.D_dtos.transactionDTO.SimpleTransactionDTO;
+import com.BBVA.DiMo_S1.D_dtos.transactionDTO.TransactionDTO;
+import jakarta.servlet.http.HttpServletRequest;
 
 
 public interface TransactionService {
 
     void sendArs(TransactionDTO transactionDTO, String token);
+
+    TransactionDTO sendUsd (HttpServletRequest request, SimpleTransactionDTO simpleTransactionDTO);
 }
