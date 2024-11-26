e0efa1b45c9192a85d97a70f5883a85223bc98bd
Juan Manuel Noli <juanmanuelnoli03@gmail.com>
2024-11-26 13:34:55 -0300

    Agregamos funcionalidad para poder actualizar el transactionLimit de una Account

--- src/main/java/com/BBVA/DiMo_S1/B_services/interfaces/AccountService.java
+++ src/main/java/com/BBVA/DiMo_S1/B_services/interfaces/AccountService.java
@@ -2,6 +2,8 @@ package com.BBVA.DiMo_S1.B_services.interfaces;
 
 import com.BBVA.DiMo_S1.D_dtos.accountDTO.AccountDTO;
 import com.BBVA.DiMo_S1.D_dtos.accountDTO.BalanceDto;
+import com.BBVA.DiMo_S1.D_dtos.accountDTO.ShowUpdateAccountDTO;
+import com.BBVA.DiMo_S1.D_dtos.accountDTO.UpdateAccountDTO;
 import com.BBVA.DiMo_S1.D_models.Account;
 import com.BBVA.DiMo_S1.E_constants.Enums.CurrencyType;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,9 +13,12 @@ public interface AccountService {
     void softDelete (final long idAccount);
 
     //2- createAccount
-    AccountDTO createAccount( final Long idUsuario, CurrencyType currencyType);
+    AccountDTO createAccount(final Long idUsuario, CurrencyType currencyType);
 
     BalanceDto obtainBalance(HttpServletRequest request);
 
     Account getAccountByEmail(String email);
+
+    //3- Actualizar Account del User
+    ShowUpdateAccountDTO updateAccount (HttpServletRequest request, UpdateAccountDTO updateAccountDTO, String cbu);
 }
