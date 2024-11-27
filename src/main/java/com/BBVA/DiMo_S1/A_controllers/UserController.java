d2498fb17d1b17a85194ac8d247889833d196fe4
Benetti <O013767@arg.bbva.com>
2024-11-27 16:50:32 -0300

    Cree script de Rol + User y fixeo rutas de transaction y users

--- src/main/java/com/BBVA/DiMo_S1/A_controllers/UserController.java
+++ src/main/java/com/BBVA/DiMo_S1/A_controllers/UserController.java
@@ -61,7 +61,7 @@ public class UserController {
         return ResponseEntity.ok(authServiceImplementation.login(userDTO.getEmail(), createUserDTO.getPassword()));
     }
 
-    @GetMapping("/users")
+    @GetMapping()
     public ResponseEntity<Page<FullUserDto>> getAll(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
