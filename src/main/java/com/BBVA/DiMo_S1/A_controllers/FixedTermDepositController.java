2a98be7b752b6052bff760a4aa3ccdee416a2335
Juan Manuel Noli <juanmanuelnoli03@gmail.com>
2024-12-02 17:36:03 -0300

    Agregamos simulacion de Plazo Fijo

--- src/main/java/com/BBVA/DiMo_S1/A_controllers/FixedTermDepositController.java
+++ src/main/java/com/BBVA/DiMo_S1/A_controllers/FixedTermDepositController.java
@@ -2,7 +2,7 @@ package com.BBVA.DiMo_S1.A_controllers;
 
 import com.BBVA.DiMo_S1.B_services.implementations.FixedTermDepositServiceImplementation;
 import com.BBVA.DiMo_S1.D_dtos.fixedTermDepositDTO.CreateFixedTermDepositDTO;
-import com.BBVA.DiMo_S1.D_dtos.fixedTermDepositDTO.FixedTermDepositDTO;
+import com.BBVA.DiMo_S1.D_dtos.fixedTermDepositDTO.ShowSimulatedFixedTermDeposit;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
@@ -30,13 +30,32 @@ public class FixedTermDepositController {
             "\n\nConsideraciones:" +
             "\n- El plazo fijo solo se puede crear mediante una cuenta en ARS." +
             "\n- El plazo fijo puede ser de 30, 60 o 90 días." +
-            "\n- El interés del mismo es del 0.2%" +
+            "\n- El interés del mismo es del 2%." +
             "\n- Lo mínimo a invertir es de $1000." +
             "\n- El monto a invertir debe ser menor al balance de la cuenta." +
             "\n- Para la liquidación del mismo, deben espetrar: 1 minuto (30 días), 2 minutos (60 días) y 3 minutos (90 días)"
     )
     @PostMapping
-    public ResponseEntity<FixedTermDepositDTO> createAccount(HttpServletRequest request, @Valid @RequestBody
+    public ResponseEntity<ShowSimulatedFixedTermDeposit> createFixedTermDeposit(HttpServletRequest request, @Valid @RequestBody
+    CreateFixedTermDepositDTO createFixedTermDepositDTO) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(fixedTermDepositServiceImplementation.
+                createFixedTermDeposit(request, createFixedTermDepositDTO));
+    }
+    //-----------------------------------------------------------------------------------------------------
+
+    //2- Simulación de un Plazo Fijo.
+    //-----------------------------------------------------------------------------------------------------
+    @Operation(summary = "Simulación de un Plazo Fijo", description = "Endpoint para simular un Plazo Fijo. " +
+            "El endpoint permite al usuario autenticado simular un Plazo Fijo." +
+            "\n\nConsideraciones:" +
+            "\n- El plazo fijo solo se puede crear mediante una cuenta en ARS." +
+            "\n- El plazo fijo puede ser de 30, 60 o 90 días." +
+            "\n- El interés del mismo es del 2%." +
+            "\n- Lo mínimo a invertir es de $1000."
+    )
+    @PostMapping("/simulate")
+    public ResponseEntity<ShowSimulatedFixedTermDeposit> simulateFixedTermDeposit(HttpServletRequest request, @Valid @RequestBody
     CreateFixedTermDepositDTO createFixedTermDepositDTO) {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(fixedTermDepositServiceImplementation.
