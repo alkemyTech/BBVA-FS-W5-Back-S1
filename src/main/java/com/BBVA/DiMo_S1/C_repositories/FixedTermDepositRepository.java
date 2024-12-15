01ca9039031c5b59675b51babeee8cd638aae027
Juan Manuel Noli <juanmanuelnoli03@gmail.com>
2024-12-15 15:06:35 -0300

    AAgregamos paginado de Plazos Fijos

--- src/main/java/com/BBVA/DiMo_S1/C_repositories/FixedTermDepositRepository.java
+++ src/main/java/com/BBVA/DiMo_S1/C_repositories/FixedTermDepositRepository.java
@@ -1,6 +1,8 @@
 package com.BBVA.DiMo_S1.C_repositories;
 
 import com.BBVA.DiMo_S1.D_models.FixedTermDeposit;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,4 +18,7 @@ public interface FixedTermDepositRepository extends JpaRepository<FixedTermDepos
 
     @Query("SELECT ftd FROM FixedTermDeposit ftd JOIN FETCH ftd.account ac")
     List<FixedTermDeposit> getFixedTermDepositsWithAccounts();
+
+    @Query("SELECT ftd FROM FixedTermDeposit ftd JOIN FETCH ftd.account ac JOIN FETCH ac.user u WHERE u.id = :idUser")
+    Page<FixedTermDeposit> getFixedTermsDepositsByIdUserPageable(@Param("idUser") long idUser, Pageable pageable);
 }
